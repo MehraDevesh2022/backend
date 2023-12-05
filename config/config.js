d1f4af5dc656b3ecb1b04ae103188f92a9eadad4
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-12-05 19:33:50 +0530

    Add user authentication functionality

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const DB_LINK ="mongodb+srv://mehradevesh2022:bG43zxYFh5CjY4GW@cluster0.u9qjfd1.mongodb.net/";
+const PORT = 8000;
+const JWT_SECRET = "secret";
+
+module.exports = {
+  DB_LINK,
+  PORT,
+  JWT_SECRET,
+};
